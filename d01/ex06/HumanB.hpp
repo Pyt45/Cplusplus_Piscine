fbc5376e717f55afc2fa5b9922ab963a8c806c38
aaqlzim <ayoub1234aqlzim@gmail.com>
2020-12-25 10:49:38 +0100

    hh

---
+++ d01/ex06/HumanB.hpp
@@ -0,0 +1,17 @@
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+
+# include "Weapon.hpp"
+
+class HumanB
+{
+    private:
+        std::string _name;
+        Weapon      *_weapon;
+    public:
+        HumanB(std::string name, Weapon *weapon);
+        void    attack() const;
+        HumanB  
+};
+
+#endif 
